b0d7d8b85fe87095d25008393765f376eea1e626
Alex <shura08g@gmail.com>
2014-10-31 13:57:23 +0200

    bascket
    Add bascket

--- SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
@@ -14,12 +14,13 @@ namespace SportsStore.WebUI.Controllers
     {
         private IProductsRepository repository;
         public int pageSize = 4;
+
         public ProductController(IProductsRepository productRepository)
         {
             this.repository = productRepository;
         }
 
-        public ViewResult List(int page = 1)
+        public ViewResult List(string category, int page = 1)
         {
             //return View(repository.Products
             //    .OrderBy(p => p.ProductID)
@@ -28,6 +29,7 @@ namespace SportsStore.WebUI.Controllers
             ProductsListViewModel model = new ProductsListViewModel
             {
                 Products = repository.Products
+                    .Where(p => category == null || p.Category == category)
                     .OrderBy(p => p.ProductID)
                     .Skip((page - 1) * pageSize)
                     .Take(pageSize),
@@ -35,8 +37,12 @@ namespace SportsStore.WebUI.Controllers
                 {
                     CurrentPage = page,
                     ItemsPerPage = pageSize,
-                    TotalItems = repository.Products.Count()
-                }
+                    // TotalItems = repository.Products.Count()
+                    TotalItems = category == null ?
+                        repository.Products.Count() :
+                        repository.Products.Where(e => e.Category == category).Count()
+                },
+                CurrentCategory = category
             };
             return View(model);
         }
